f73dc082df36a93295007223e65e1476b2d80be8
Robert-Lauriston-at-Striim <94200129+Robert-Lauriston-at-Striim@users.noreply.github.com>
2026-04-29 10:59:01 +0200

    Add files via upload

---
+++ pg_ddl_setup.sql
@@ -0,0 +1,46 @@
+-- STRIIM POSTGRESQL DDL CAPTURE SETUP
+-- Execute this script through psql console.
+-- CMD: psql <rolename> -h <hostname or hostIP> -d <DatabaseName> -f -a </path/to>pg_ddl_setup.sql 
+
+CREATE SCHEMA IF NOT EXISTS striim;
+
+CREATE TABLE IF NOT EXISTS striim.ddlcapturetable ( event text,
+tag text, classid oid, objid oid, objsubid int,
+object_type text, schema_name text, object_identity text, is_extension bool, query text,
+username text default current_user, db_name text default current_database(),
+client_addr inet default inet_client_addr(), creation_time timestamp default now()
+);
+
+GRANT USAGE ON SCHEMA striim TO PUBLIC;
+GRANT SELECT, INSERT ON TABLE striim.ddlcapturetable TO PUBLIC;
+
+create or replace function striim.ddl_capture_command() returns event_trigger as $$
+declare r record;
+begin
+    if TG_EVENT='ddl_command_end' then
+        for r IN
+            SELECT * FROM pg_event_trigger_ddl_commands()
+        loop
+            if r.classid > 0 then
+            insert into striim.ddlcapturetable(event, tag, classid, objid, objsubid, object_type, schema_name, object_identity, is_extension, query)
+            values(TG_EVENT, TG_TAG, r.classid, r.objid, r.objsubid, r.object_type, r.schema_name, r.object_identity, r.in_extension, current_query());
+         end if;
+        end loop;
+    end if;
+     if TG_EVENT='sql_drop' then
+        for r IN
+            SELECT * FROM pg_event_trigger_dropped_objects()
+        loop
+            insert into striim.ddlcapturetable(event, tag, classid, objid, objsubid, object_type, schema_name, object_identity, is_extension, query)
+            values(TG_EVENT, TG_TAG, r.classid, r.objid, r.objsubid, r.object_type, r.schema_name, r.object_identity, 'f', current_query());
+        end loop;
+    end if;
+end;
+$$ language plpgsql strict;
+
+DROP event TRIGGER pg_get_ddl_command; 
+DROP event TRIGGER pg_get_ddl_drop;
+
+
+CREATE EVENT TRIGGER pg_get_ddl_command on ddl_command_end EXECUTE PROCEDURE striim.ddl_capture_command();
+CREATE EVENT TRIGGER pg_get_ddl_drop on sql_drop EXECUTE PROCEDURE striim.ddl_capture_command();
